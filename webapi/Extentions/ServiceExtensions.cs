74241860c1cfa16cbc12b7fc51da58381a6a7631
fizlrock <nirku0soft@yandex.ru>
2023-11-11 00:24:26 +0400

    чуть-чуть коментариев

--- webapi/Extentions/ServiceExtensions.cs
+++ webapi/Extentions/ServiceExtensions.cs
@@ -3,6 +3,7 @@ public static class ServiceExtensions
 {
     public static void ConfigureCors(this IServiceCollection services)
     {
+			// CORS - механизм ограничения доступа к сервису
         services.AddCors(
                         options =>
                         {
