65fd52607a2f3305ee6f5d9776808da20dc94d00
fizlrock <nirku0soft@yandex.ru>
2023-11-11 17:39:28 +0400

    форматирование

--- Domain/Class1.cs
+++ Domain/Class1.cs
@@ -2,27 +2,27 @@
 public class Class1
 {
     //Перевод строки из Полиз в инфикс
-    public static string fromPolisToInfics (string polis)
+    public static string fromPolisToInfics(string polis)
     {
         // if (isPolis(polis))
         // {
-            
+
         // }
         // else
         // {
-            
+
         // }
         throw new NotImplementedException();
     }
     //Перевод из инфикс в Полиз
-    public static string fromInficsToPolis (string infics)
+    public static string fromInficsToPolis(string infics)
     {
         throw new NotImplementedException();
     }
     //Проверка на наличие лишних символов
-    private bool isPolis (string polis)
+    private bool isPolis(string polis)
     {
-        throw new NotImplementedException();        
+        throw new NotImplementedException();
     }
 
     public static double calculate(string polis)
