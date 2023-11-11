d7d2a659b9a73a6ef5d99d34c4b369981175e5a3
lerakaryan <lera.karyan14@mail.ru>
2023-11-11 17:31:12 +0400

    методы

--- Domain/Class1.cs
+++ Domain/Class1.cs
@@ -1,5 +1,37 @@
 ﻿namespace Domain;
 public class Class1
 {
+    //Перевод строки из Полиз в инфикс
+    public static string fromPolisToInfics (string polis)
+    {
+        // if (isPolis(polis))
+        // {
+            
+        // }
+        // else
+        // {
+            
+        // }
+        throw new NotImplementedException();
+    }
+    //Перевод из инфикс в Полиз
+    public static string fromInficsToPolis (string infics)
+    {
+        throw new NotImplementedException();
+    }
+    //Проверка на наличие лишних символов
+    private bool isPolis (string polis)
+    {
+        throw new NotImplementedException();        
+    }
+
+    public static double calculate(string polis)
+    {
+        throw new NotImplementedException();
+    }
+    public static double calculate(string infics)
+    {
+        throw new NotImplementedException();
+    }
 
 }
