2fe7630551f72b294f7f3c75b4ec969815cf63d8
lerakaryan <lera.karyan14@mail.ru>
2023-11-12 18:13:36 +0400

    логика

--- Domain/MathUtils.cs
+++ Domain/MathUtils.cs
@@ -1,6 +1,8 @@
 ﻿namespace Calc.Models;
 public class MathUtils
 {
+    private string [] lib  = {'1','2','3','4','5','6','7','8','9','0','(',')','+','-','*','/'};
+
     //Перевод строки из Полиз в инфикс
     public static string fromPolisToInfics(string polis)
     {
@@ -14,15 +16,55 @@ public class MathUtils
         // }
         throw new NotImplementedException();
     }
+    
+    private static class Table (string oper1, int weight1)
+    {
+        string oper = oper1;
+        int weight = weight1;
+    } 
+
+    private static bool isNumber (string a)
+    {
+        int x = 0;
+        if (Int32.TryParse(a, out x))
+            return true;
+    }
     //Перевод из инфикс в Полиз
     public static string fromInficsToPolis(string infics)
     {
+        Table[] check = new Table[]{
+            ('(', 10), 
+            (')', 0), 
+            ('+', 1), 
+            ('-', 1), 
+            ('*', 2), 
+            ('/', 2) };
+
+        string [] line = infics.split([separator[]]);
+
+        if(isPolis(infics))
+        {
+            
+        }
+
         throw new NotImplementedException();
     }
-    //Проверка на наличие лишних символов
+    //Проверка строки на наличие лишних символов, лучше назвать isCorrect
     private bool isPolis(string polis)
     {
-        throw new NotImplementedException();
+        bool a = 0;
+        string [] line = infics.split([separator[]]);
+        for (int i = 0; i<line.Length; i++)
+        {
+            for (int j = 0; j<lib.Length; i++)
+            {
+                if (line[i] == lib[j])
+                    a = 1;
+            }
+            if (a == 0)
+                break;
+        }
+        return a;
     }
     public static double calculate(string infics)
     {
@@ -30,3 +72,4 @@ public class MathUtils
     }
 
 }
+//throw new NotImplementedException();
